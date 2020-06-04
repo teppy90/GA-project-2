f1a810c46caf8a9fc71f48c0cafa3200de5d2421
teppy90 <jared.tai90@gmail.com>
2020-06-04 14:38:48 +0800

    created new author route for (get,create), fixed env function typeerror

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load()
+    require('dotenv').config();
 }
 
 const express = require('express')
@@ -7,6 +7,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -15,13 +16,13 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { 
-useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
