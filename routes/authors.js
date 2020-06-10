4ac9b5813039d6f6553cb8a768ddfaeb4749519f
teppy90 <jared.tai90@gmail.com>
2020-06-11 03:13:34 +0800

    book schema routes and halfway through views

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,10 @@ router.get('/', async (req, res) => {
     }
     try {
       const authors = await Author.find(searchOptions)
-      res.render('authors/index', { authors: authors, searchOptions: req.query })
+      res.render('authors/index', { 
+        authors: authors, 
+        searchOptions: req.query 
+      })
     } catch {
         res.redirect('/')
     }
