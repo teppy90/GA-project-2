429c69e4e6399a9f818e7f851e0e27bf54f9e260
teppy90 <jared.tai90@gmail.com>
2020-06-18 00:22:20 +0800

    finish book routes before debug

--- routes/books.js
+++ routes/books.js
@@ -26,10 +26,6 @@ router.get('/', async (req,  res) => {
   }
 })
 
-// New Book Route
-router.get('/new', async (req, res) => {
-  renderNewPage(res, new Book())
-})
 
 // Create Book Route
 router.post('/', async (req, res) => {
@@ -44,27 +40,90 @@ router.post('/', async (req, res) => {
 
     try {
       const newBook = await book.save()
-      res.redirect(`books`)
+      res.redirect(`books/${newBook.id}`)
 
     } catch {
       renderNewPage(res, book, true)
     }
   })
 
+// Show Book Route
+router.get('/:id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+                           .populate('author')
+                           .exec()
+    res.render('books/show', { book: book })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit Book Route
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id)
+  renderEditPage(res, book)
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Update Book Route
+router.put('/:id', async (req, res) => {
+  let book
+
+    try {
+      book = Book.findById(req.params.id)
+      book.title = req.body.title
+      book.author = req.body.author
+      book.publishDate = new Date(req.body.publishDate)
+      book.PageCount = req.body.pageCount
+      book.description = req.body.description
+      if (req.body.cover != null && req.body.cover != '') {
+        saveCover(book, req.body.cover)
+      }
+      await book.save()
+      res.redirect(`books/${book.id}`)
+    } catch {
+      if (book != null) {
+        renderEditPage(res, book, true)
+      } else {
+        redirect('/')
+      }    
+    }
+  
 async function renderNewPage(res, book, hasError = false) {
+  renderFormPage(res, book, 'new', hasError)
+}
+
+
+async function renderEditPage(res, book, hasError = false) {
+  renderFormPage(res, book, 'edit', hasError)
+}
+
+
+async function renderFormPage(res, book, form, hasError = false) {
   try {
       const authors = await Author.find({})
       const params = {
         authors: authors,
         book: book
       }
-      if (hasError) params.errorMessage = 'Error Creating Book'
-      res.render('books/new', params)        
-    } catch {
+      if (hasError) {
+        if (form === 'edit') {
+          params.errorMessage= 'Error Updating Book'
+        } else {
+          params.errorMessage 'Error Creating Book'
+        }
+      }
+      res.render(`books/${form}`, params)        
+      } catch {
         res.redirect('/books')
+      }
     }
-  }
-
+  
 function saveCover(book, coverEncoded) {
   if (coverEncoded == null) return
   const cover = JSON.parse(coverEncoded)
